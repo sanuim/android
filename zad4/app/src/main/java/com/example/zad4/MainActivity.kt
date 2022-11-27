6bde47f68d532e2bc90ea023ecfb00cd5d41ffa0
sanuim <agatap17@outlook.com>
2022-11-28 00:20:06 +0100

    zad 4 for 3.0

--- zad4/app/src/main/java/com/example/zad4/MainActivity.kt
+++ zad4/app/src/main/java/com/example/zad4/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.zad4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setFragment(Tasks())
+    }
+
+    fun setFragment(f: Fragment) {
+
+        val ft = supportFragmentManager.beginTransaction()
+
+        ft.replace(R.id.framelayout, f)
+        ft.commit()
     }
 }
--- zad4/app/src/main/java/com/example/zad4/Tasks.kt
+++ zad4/app/src/main/java/com/example/zad4/Tasks.kt
@@ -2,63 +2,35 @@ package com.example.zad4
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import com.example.zad4.placeholder.PlaceholderContent
-import kotlinx.android.synthetic.main.fragment_tasks_list.view.*
 
 class Tasks : Fragment() {
-
-    private var columnCount = 1
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(Tasks.ARG_COLUMN_COUNT)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_tasks_list, container, false)
-
-        val myList = ArrayList<String>()
-        myList.add("Zadanie 1")
-//        // Set the adapter
-//        if (view is RecyclerView) {
-//            with(view) {
-//                layoutManager = when {
-//                    columnCount <= 1 -> LinearLayoutManager(context)
-//                    else -> GridLayoutManager(context, columnCount)
-//                }
-//                adapter = MyItemRecyclerViewAdapter(myList)
-//            }
-//        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_tasks, container, false)
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
+        val context = context as MainActivity
 
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            Tasks().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
+        val arrayList: ArrayList<String> = ArrayList()
+        arrayList.add("Zadanie 1")
+        arrayList.add("Zadanie 2")
+        arrayList.add("Zadanie 3")
+
+        val lv = context.findViewById(android.R.id.list) as ListView
+        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, arrayList)
+        lv.adapter = adapter
     }
+
+
 }
